ef5ed9fc48414a3103e397d04ada59717c57e3ec
corang <jordanmc12@verizon.net>
2016-02-01 13:54:39 -0500

    work at school

---
+++ While Loops/src/AdventureTwo.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jormcc16@shasd.org on 2/1/16.
+ */
+public class AdventureTwo {
+}
--- While Loops/src/CollatzSequence.java
+++ While Loops/src/CollatzSequence.java
@@ -1,10 +1,37 @@
 /**
  * Created by Jorda on 1/31/2016.
  */
-public class CollatzSequence
-{
-    public static void main(String[] args)
-    {
 
+import java.util.Scanner;
+
+public class CollatzSequence {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        double num, tries;
+
+        tries = 0;
+
+        System.out.println("Collatz Sequence");
+        System.out.print("Enter Number that is not 1: ");
+        num = keyboard.nextDouble();
+
+        while (num != 1)
+        {
+            if ((num % 2) == 0)
+            {
+                num = num / 2;
+                System.out.println(num);
+            }
+            else
+            {
+                num = (num * 3) + 1;
+                System.out.println(num);
+            }
+            tries ++;
+
+        }
+
+        System.out.println("\n" + tries);
     }
 }
