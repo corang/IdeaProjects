d5beb29e305b33bd8c3bef0d3c58395eb8f8c890
corang <jordanmc12@verizon.net>
2016-02-01 14:07:29 -0500

    more

--- While Loops/src/AdventureTwo.java
+++ While Loops/src/AdventureTwo.java
@@ -1,5 +1,46 @@
 /**
  * Created by jormcc16@shasd.org on 2/1/16.
  */
-public class AdventureTwo {
+import java.util.Scanner;
+
+public class AdventureTwo
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        String choice = "";
+        int room = 1;
+
+        while (room != 0)
+        {
+            if (room == 1)
+            {
+                System.out.println("You find yourself in a room.");
+
+            }
+            else if (room == 2)
+            {
+
+            }
+            else if (room == 3)
+            {
+
+            }
+            else if (room == 4)
+            {
+
+            }
+            else if (room == 5)
+            {
+
+            }
+            else if (room == 6)
+            {
+
+            }
+        }
+
+        System.out.print("Game Over.");
+    }
 }
